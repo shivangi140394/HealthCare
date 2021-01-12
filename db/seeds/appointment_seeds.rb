700e5a9116a374553fd170e5a7ca8fe937351903
Shivangi140394 <shivangi14shrivastava@gmail.com>
2021-01-12 10:57:38 +0530

    changes in seed data

--- db/seeds/appointment_seeds.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true'
-
-Appointment.create(patient_id: 1, therapist_id: 1, status: 'book')
-Appointment.create(patient_id: 1, therapist_id: 1, status: 'confirm')
-Appointment.create(patient_id: 2, therapist_id: 2, status: 'cancle')
-Appointment.create(patient_id: 2, therapist_id: 2, status: 'confirm')
--- db/seeds/users_seeds.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-User.create(id: 1, email: 'john@gmail.com', password: '123123123', role: 'therapist')
-User.create(id: 2, email: 'bob@gmail.com', password: '123123123', role: 'therapist')
-User.create(id: 3, email: 'patient@gmail.com', password: '123123123', role: 'patient')
-User.create(id: 4, email: 'leena@gmail.com', password: '123123123', role: 'patient')
