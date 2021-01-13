4b357d062e53cb7ddba7cc88a4fc034c99be14bb
Shivangi140394 <shivangi14shrivastava@gmail.com>
2021-01-13 13:27:17 +0530

    changes in dashboard view

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,8 +5,7 @@ class DashboardController < ApplicationController
     current_user.role.patient? ? Patient.find_by(user_id: current_user.id) : Therapist.find_by(user_id: current_user.id)
     @patients = Patient.all
     @therapists = Therapist.all
-    @current_patient = Patient.find_by_user_id(current_user.id)
-    @current_therapist = Therapist.find_by_user_id(current_user.id)
+    current_login_user
     @appointments = Appointment.where(therapist_id: @current_therapist)
   end
 end
